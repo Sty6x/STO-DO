08f28992481fbf9e61c530fb66ec30695d5cd76c
Cheesuscrust460 <Jerylfranz460@gmail.com>
2022-09-08 02:33:31 +0400

    finished adding every necessities for project

---
+++ dist/main.bundle.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('something here')
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5idW5kbGUuanMiLCJtYXBwaW5ncyI6Ijs7Ozs7QUFBQSw2QiIsInNvdXJjZXMiOlsid2VicGFjazovL3N0by1kby8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZygnc29tZXRoaW5nIGhlcmUnKSJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,21 @@
-const path = require('path')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode:'development',
-    entry:'./src/index.js',
-    output:{
-        filename:'main.js',
-        path: path.resolve(__dirname,'dist'),
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
         clean: true,
     },
-    devtool:'inline-source-map',
+    devtool: 'inline-source-map',
     devServer: {
         static: './dist',
-      },
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Output Management',
+        }),
+    ],
 }
