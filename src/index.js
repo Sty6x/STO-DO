2dec2c6370088598664b367a33a407abe38cada9
zaid <jerylfranz460@gmail.com>
2022-09-12 07:21:08 +0400

    installed css Loader and imported in index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import PubSub from "pubsub-js"
+import './style.css';
+
 
 const h1 = document.querySelector('h1')
 h1.textContent='dawdawdw'
--- webpack.config.js
+++ webpack.config.js
@@ -5,14 +5,20 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     mode: 'development',
     entry: {
-        index:'./src/index.js',
-        Data:'./src/Data.js'
+        index: './src/index.js',
+        Data: './src/Data.js'
     },
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
         publicPath: '/',
+    },
+    module: {
+        rules: [{
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+        }, ],
     },
     devtool: 'inline-source-map',
     devServer: {
