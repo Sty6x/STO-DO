a11d90c112fb6d7c156d0c53ab3b47f8993541c0
zaid <jerylfranz460@gmail.com>
2022-09-12 12:55:04 +0400

    added an event listener to sidebar to change active and inactive status

--- src/index.js
+++ src/index.js
@@ -7,9 +7,18 @@ function getData(msg,{name,age,favoriteFood}){
 PubSub.subscribe('getData',getData)
 
 
+const menuButton = document.getElementById('hamburger-menu')
+const sideBarContainer = document.getElementById('sidebar-container')
+const sideBar = document.querySelector('.sidebar')
 function changeSidebar(){
-
+    if(!sideBarContainer.classList.contains('sidebar-active')){
+        console.log('has')
+        sideBarContainer.setAttribute('class','sidebar-active')
+        sideBarContainer.appendChild(sideBar)
+    }else{
+        sideBarContainer.setAttribute('class','sidebar-inactive')
+        sideBarContainer.removeChild(sideBar)
+    }
 }
 
-const menuButton = document.getElementById('hamburger-menu')
 menuButton.addEventListener('click', changeSidebar)
