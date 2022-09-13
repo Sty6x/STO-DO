5591ed072f6c1fb3d30f6b2367adff25821aef6a
zaid <jerylfranz460@gmail.com>
2022-09-13 10:43:42 +0400

    added Dom elements for today's task and appeneded then gave each element an id

--- src/createMainUI.js
+++ src/createMainUI.js
@@ -3,6 +3,30 @@ const sbMainContainer = document.getElementById('sb-main-container')
 const mainContentContainer = document.getElementById('main-content-container')
 
 
-console.log(body)
-console.log(sbMainContainer)
-console.log(mainContentContainer)
+const headerContainer = document.createElement('div')
+const todayHeader = document.createElement('h1')
+const headerOptionContainer = document.createElement('ul')
+
+
+const tdTaskContainer = document.createElement('div')
+
+const optionArr = []
+for (let i = 0; i < 3; i++) {
+    let option = document.createElement('li')
+    optionArr.push(option)
+    optionArr[i].textContent = `${i}`
+    headerOptionContainer.append(optionArr[i])
+}
+
+
+function createTdTask() {
+    mainContentContainer.appendChild(headerContainer)
+    mainContentContainer.appendChild(tdTaskContainer)
+    tdTaskContainer.setAttribute('id','td-task-container')
+    headerContainer.append(todayHeader)
+    headerContainer.appendChild(headerOptionContainer)
+    headerContainer.setAttribute('id','header-container')
+    headerOptionContainer.setAttribute('id','header-option-container')
+    todayHeader.textContent = "TODAY'S TASK"
+}
+createTdTask()
