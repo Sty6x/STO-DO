044005bebee3d4797b853b17d7242fe25282d543
zaid <jerylfranz460@gmail.com>
2022-09-14 08:41:30 +0400

    removed created nodes and instead referenced the header from markup instead of having its own header to incorporate the same stylees for every module header

--- src/createMainUI.js
+++ src/createMainUI.js
@@ -3,37 +3,28 @@ const sbMainContainer = document.getElementById('sb-main-container')
 const mainContentContainer = document.getElementById('main-content-container')
 
 
-const headerContainer = document.createElement('div')
-const todayHeader = document.createElement('h1') //style
-const headerOptionContainer = document.createElement('ul')
 
+
+const headerContainer = document.getElementById('header-container')
+const todayHeader = document.getElementById('header-text') //style
+const headerOptionContainer = document.getElementById('header-option-container')
+const options = headerOptionContainer.children;
+console.log(options)
+//get header options children
+
+for(let i = 0;i < options.length;i++){
+    options[i].setAttribute('id',`option-${i}`)
+    options[i].classList.add('option-icons')    
+}
 
 const tdMainContainer = document.createElement('div')
 const tdTasksContainer = document.createElement('div')
 const addTaskbutton = document.createElement('button')
 
-const optionArr = [] //style
-for (let i = 0; i < 2; i++) {
-    let option = document.createElement('li')
-    optionArr.push(option)
-    // optionArr[i].textContent = `${i}`
-    headerOptionContainer.append(optionArr[i]) //style
-    optionArr[i].setAttribute('class',`option-${i}`)
-    optionArr[i].classList.add('option-icons')
-}
-
-
 function createTdTask() {
-    mainContentContainer.appendChild(headerContainer)
-    headerContainer.setAttribute('id','header-container')
     mainContentContainer.appendChild(tdMainContainer)
-    tdMainContainer.setAttribute('id','td-main-task-container')
+    tdMainContainer.setAttribute('id', 'td-main-task-container')
     tdMainContainer.appendChild(tdTasksContainer)
-    tdTasksContainer.setAttribute('id','td-task-container')
-    headerContainer.append(todayHeader)
-    todayHeader.setAttribute('id','today-header') //style
-    headerContainer.appendChild(headerOptionContainer)
-    headerOptionContainer.setAttribute('id','header-option-container')
     todayHeader.textContent = "TODAY"
 }
 createTdTask()
