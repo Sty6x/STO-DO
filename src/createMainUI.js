de9446c4f750c3338cb273ae294bbc392ac7108b
diaz <jerylfranz460@gmail.com>
2022-09-14 13:17:47 +0400

    added a function that creates a task and appends it into our tdmaincontainer

--- src/createMainUI.js
+++ src/createMainUI.js
@@ -3,14 +3,11 @@ const sbMainContainer = document.getElementById('sb-main-container')
 const mainContentContainer = document.getElementById('main-content-container')
 
 
-
-
 const headerContainer = document.getElementById('header-container')
 const todayHeader = document.getElementById('header-text') //style
 const headerOptionContainer = document.getElementById('header-option-container')
 const options = headerOptionContainer.children;
-console.log(options)
-//get header options children
+
 
 for(let i = 0;i < options.length;i++){
     options[i].setAttribute('id',`option-${i}`)
@@ -22,7 +19,7 @@ const tdTasksContainer = document.createElement('div')
 const addTaskbutton = document.createElement('button')
 const addTaskContainer = document.createElement('div')
 
-function createTdTask() {
+function createTdTaskUI() {
     mainContentContainer.appendChild(tdMainContainer)
     tdMainContainer.setAttribute('id', 'td-up-main-task-container')
     todayHeader.textContent = "TODAY"
@@ -36,4 +33,28 @@ function createTdTask() {
 addTaskbutton.addEventListener('click',()=> {
     console.log('here')
 })
-createTdTask()
+createTdTaskUI()
+
+
+// creates task from user input
+function createTasks(tdContainer){
+    let task = document.createElement('div');
+    // container => |     |title:      |                |    |
+    //              |     |Description:|                |o o o| <= container
+    task.setAttribute('id','task');
+    // let checkBox = document.createElement('')
+    let taskTitle = document.createElement('h3');
+    let taskDesc = document.createElement('p')
+    let titleAndDescContainer = document.createElement('div')
+    let taskSettings = document.createElement('div')
+    task.appendChild(titleAndDescContainer)
+    tdContainer.appendChild(task)
+    titleAndDescContainer.append(taskTitle,taskDesc,taskSettings)
+    taskTitle.textContent='Add Title Here'
+    taskDesc.textContent='Make it do something, i dont care'
+}
+
+
+createTasks(tdMainContainer)
+
+createTasks(tdMainContainer)
