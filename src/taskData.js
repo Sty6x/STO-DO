e4b94e2b52fac41618073b6858e1e9b14c546549
zaid <jerylfranz460@gmail.com>
2022-09-16 09:14:37 +0400

    still finding a way how to make priority buttons independent and just pass in the value to GetUserInput function

--- src/taskData.js
+++ src/taskData.js
@@ -2,45 +2,41 @@ const priorityButtonContainer = document.getElementById('form-priority-buttons')
 const userDesc = document.getElementById('desc')
 const userTitle = document.getElementById('title')
 const inputContainer = document.querySelectorAll('.input-container')
+const addTaskButton = document.getElementById('form-add-button');
 
+function GetUserInput() {
+    let descInput;
+    let titleInput;
+    descInput = userDesc.value;
+    console.log(descInput)
+    titleInput = userTitle.value;
+    console.log(titleInput)
+    let getPriority;
 
-function getUserInput(){    
-let descInput;
-let titleInput;
-inputContainer.forEach(input => {
-    input.addEventListener('keypress',()=>{
-        descInput = userDesc.value;
-        console.log(descInput)
-        titleInput = userTitle.value;
-        console.log(titleInput)
-    })
-});
-return{
-    descInput,
-    titleInput
+    priorityButtonContainer.addEventListener('click', e => {
+            console.log(e.target)
+            if (e.target.matches('#very-important')) {
+                console.log('very important')
+                getPriority = '#bf616a';
+            } else if (e.target.matches('#important')) {
+                console.log('important')
+                getPriority = '#ebcb8b';
+            } else if (e.target.matches('#less-important')) {
+                console.log('less important')
+                getPriority = '#a3be8c';
+            }
+
+        })
+    return {
+        descInput,
+        titleInput,
+        getPriority
+    }
+
 }
 
-}
-
-function getPriority(e) {
-    let priorityColor;
-    if (e.target.matches('#very-important')) {
-        console.log('very important')
-        priorityColor = '#bf616a';
-    }
-    else if (e.target.matches('#important')) {
-        console.log('important')
-        priorityColor = '#ebcb8b';
-    }
-    else if (e.target.matches('#less-important')) {
-        console.log('less important')
-        priorityColor = '#a3be8c';
-    }
-    return priorityColor
-}
-
-priorityButtonContainer.addEventListener('click', e => {
+addTaskButton.addEventListener('click', e => {
+    e.preventDefault();
     console.log(e.target)
-    console.log(getPriority(e))
-
+    console.log(GetUserInput())
 })
