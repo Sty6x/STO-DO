99ec67a0ed9db5c08e51240ada3d4e32a6f40abe
zaid <jerylfranz460@gmail.com>
2022-09-16 08:33:07 +0400

    rebuilt to apply logic for taskData and TaskUI

---
+++ taskData.bundle.js
@@ -0,0 +1,18 @@
+(self["webpackChunksto_do"] = self["webpackChunksto_do"] || []).push([["taskData"],{
+
+/***/ "./src/taskData.js":
+/*!*************************!*\
+  !*** ./src/taskData.js ***!
+  \*************************/
+/***/ (() => {
+
+
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/taskData.js"));
+/******/ }
+]);
