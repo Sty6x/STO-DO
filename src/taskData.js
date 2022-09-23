97c1d46c8facbf27fa954c1284dc26c726fd918c
zaid <jerylfranz460@gmail.com>
2022-09-23 14:40:23 +0400

    test commit

--- src/taskData.js
+++ src/taskData.js
@@ -9,65 +9,65 @@ const formAddTaskButton = document.getElementById('form-add-button');
 
 let priority;
 priorityButtonContainer.addEventListener('click', event => {
-    console.log(event.target)
-    if (event.target.matches('#very-important')) {
-        console.log('very important')
-        priority = '#bf616a';
-    } else if (event.target.matches('#important')) {
-        console.log('important')
-        priority = '#ebcb8b';
-    } else if (event.target.matches('#less-important')) {
-        console.log('less important')
-        priority = '#a3be8c';
-    }
+  console.log(event.target)
+  if (event.target.matches('#very-important')) {
+    console.log('very important')
+    priority = '#bf616a';
+  } else if (event.target.matches('#important')) {
+    console.log('important')
+    priority = '#ebcb8b';
+  } else if (event.target.matches('#less-important')) {
+    console.log('less important')
+    priority = '#a3be8c';
+  }
 })
 
 
 
 function clearInput() {
-    userDesc.value = '';
-    userTitle.value = '';
-    // priority = '';
+  userDesc.value = '';
+  userTitle.value = '';
+  // priority = '';
 };
 
 
 // why do i need this?
 const userTasks = [{}]
 function storeUserTask(obj) {
-    userTasks.push(obj)
-    console.log(userTasks)
+  userTasks.push(obj)
+  console.log(userTasks)
 };
 
 
 function getUserData() {
-    let descInput;
-    let titleInput;
-    let id;
-    descInput = userDesc.value;
-    titleInput = userTitle.value;
-    for(let i = 0; i < userTasks.length; i++){
-        id = `${i}`;
-    }
-    if (priority == ''){
-        priority = 'var(--dark)';
-    }
-    return {
-        titleInput,
-        descInput,
-        priority,
-        id
-    };
+  let descInput;
+  let titleInput;
+  let id;
+  descInput = userDesc.value;
+  titleInput = userTitle.value;
+  for (let i = 0; i < userTasks.length; i++) {
+    id = `${i}`;
+  }
+  if (priority == '') {
+    priority = 'var(--dark)';
+  }
+  return {
+    titleInput,
+    descInput,
+    priority,
+    id
+  };
 };
 
 formAddTaskButton.addEventListener('click', e => {
-    e.preventDefault();
-    // use this logic to throw an error if input is empty
-    if(userTitle.value == ''){
-        console.log('need title');
-        return;
-    }else{
-        PubSub.publish('getTaskData',getUserData())
-        storeUserTask(getUserData());
-        clearInput();
-    }
+  e.preventDefault();
+  // use this logic to throw an error if input is empty
+  if (userTitle.value == '') {
+    console.log('need title');
+    return;
+  } else {
+    PubSub.publish('getTaskData', getUserData())
+    storeUserTask(getUserData());
+    clearInput();
+  }
 });
