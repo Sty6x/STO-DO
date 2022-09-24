4bf81a6bdfb0aa64e07fbc98d1b285d7b2b2d569
diaz <jerylfranz460@gmail.com>
2022-09-24 17:24:46 +0400

    rebuilt to delete data.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,45 +3,44 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 // var template = require("./file.handlebars");
 
 module.exports = {
-    mode: 'development',
-    entry: {
-        index: './src/index.js',
-        data: './src/data.js',
-        taskUi: './src/taskUI.js',
-        taskData: './src/taskData.js'
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+    taskUi: './src/taskUI.js',
+    taskData: './src/taskData.js'
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    publicPath: '/',
+  },
+  module: {
+    rules: [{
+      test: /\.css$/i,
+      use: ['style-loader', 'css-loader'],
+    }, {
+      test: /\.(png|svg|jpg|jpeg|gif)$/i,
+      type: 'asset/resource',
     },
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-        publicPath: '/',
+    {
+      test: /\.(woff|woff2|eot|ttf|otf)$/i,
+      type: 'asset/resource',
     },
-    module: {
-        rules: [{
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            }, {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                type: 'asset/resource',
-            },
-        ],
-
-    },
-    devtool: 'inline-source-map',
-    devServer: {
-        static: './src',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Sto-Do',
-            template: '!!handlebars-loader!src/index.hbs'
-        }),
     ],
-    optimization: {
-        runtimeChunk: 'single'
-    },
+
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './src',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Sto-Do',
+      template: '!!handlebars-loader!src/index.hbs'
+    }),
+  ],
+  optimization: {
+    runtimeChunk: 'single'
+  },
 }
