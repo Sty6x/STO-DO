66efea01826837fa1259f99903b11d157df7fa6c
diaz <jerylfranz460@gmail.com>
2022-09-24 17:25:55 +0400

    deleted uneeded file, removed the function that creates a fomr within the title and description container

--- src/data.js
+++
@@ -1,9 +0,0 @@
-import PubSub from "pubsub-js"
-
-console.log('human music this is actualy pretty nice')
-const person = {
-  name: 'fra',
-  age: 32,
-  favoriteFood: 'dwa'
-}
-PubSub.publish('getData', person)
--- src/index.js
+++ src/index.js
@@ -1,24 +1,18 @@
 import PubSub from "pubsub-js"
 import './style.css';
 
-function getData(msg,{name,age,favoriteFood}){
-    console.log(msg,{name:name,age:age,Favoritefood:favoriteFood})
-}
-PubSub.subscribe('getData',getData)
-
-
 const menuButton = document.getElementById('hamburger-menu')
 const sideBarContainer = document.getElementById('sidebar-container')
 const sideBar = document.querySelector('.sidebar')
-function changeSidebar(){
-    if(!sideBarContainer.classList.contains('sidebar-active')){
-        console.log('has')
-        sideBarContainer.setAttribute('class','sidebar-active')
-        sideBarContainer.appendChild(sideBar)
-    }else{
-        sideBarContainer.setAttribute('class','sidebar-inactive')
-        sideBarContainer.removeChild(sideBar)
-    }
+function changeSidebar() {
+  if (!sideBarContainer.classList.contains('sidebar-active')) {
+    console.log('has')
+    sideBarContainer.setAttribute('class', 'sidebar-active')
+    sideBarContainer.appendChild(sideBar)
+  } else {
+    sideBarContainer.setAttribute('class', 'sidebar-inactive')
+    sideBarContainer.removeChild(sideBar)
+  }
 }
 
 menuButton.addEventListener('click', changeSidebar)
