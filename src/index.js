776088d0425b0c8262dea898b52a675160f906fc
diaz <jerylfranz460@gmail.com>
2022-09-26 15:44:11 +0400

    gave date in header

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import PubSub from "pubsub-js"
 import './style.css';
-
+import { format } from 'date-fns'
 const menuButton = document.getElementById('hamburger-menu')
 const sideBarContainer = document.getElementById('sidebar-container')
 const sideBar = document.querySelector('.sidebar')
+let headerDate = format(new Date(), 'Et  MMM dd')
+const headerTextDateContainer = document.getElementById('header-text-date-container')
+const headerDateDisplay = document.createElement('small')
+headerDateDisplay.textContent = headerDate;
+headerTextDateContainer.appendChild(headerDateDisplay)
+headerTextDateContainer.setAttribute('style', 'display:flex; gap:10px;')
+headerDateDisplay.setAttribute('style', 'align-self:end; font-family:"Montserrat-Reg"; color: var(--text)')
 function changeSidebar() {
   if (!sideBarContainer.classList.contains('sidebar-active')) {
     console.log('has')
