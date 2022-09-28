37c67ffc4bb80eda99999ae2f13bbccb583bed20
diaz <jerylfranz460@gmail.com>
2022-09-28 18:14:33 +0400

    removed unused code

--- src/todayUI.js
+++ src/todayUI.js
@@ -1,9 +1,7 @@
 import * as TaskUI from '/src/taskUI.js'
 
-const body = document.querySelector("body");
 const sbMainContainer = document.getElementById("sb-main-container");
 const mainContentContainer = document.getElementById("main-content-container");
-const headerContainer = document.getElementById("header-container");
 const todayHeader = document.getElementById("header-text"); //style
 const headerOptionContainer = document.getElementById("header-option-container");
 const options = headerOptionContainer.children;
@@ -14,18 +12,10 @@ for (let i = 0; i < options.length; i++) {
 }
 
 const tdMainContainer = document.createElement("div");
-const tdTasksContainer = document.createElement("div");
 const addTaskbutton = document.createElement("button");
 const addTaskContainer = document.createElement("div");
 
-// const dimBg = document.createElement("div");
-// dimBg.setAttribute(
-//   "style",
-//   "position:absolute;bottom:0%;left:0%;z-index:99;min-height:100vh;width:100vw;background-color:black;opacity:.5"
-// );
-// dimBg.setAttribute('id', 'dim-background-form')
-
-// creates the body of Today task
+/// creates the body of Today task
 function createTdTaskUI() {
   mainContentContainer.appendChild(tdMainContainer);
   tdMainContainer.setAttribute("id", "td-up-main-task-container");
