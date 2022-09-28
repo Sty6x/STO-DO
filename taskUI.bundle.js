489eb4faa608717dd333c83dfb792f3010617735
diaz <jerylfranz460@gmail.com>
2022-09-28 17:17:40 +0400

    added new entry points and additional stylesheet for corresponding UIs and refactored some functions that would take in a container when we use them

---
+++ taskUI.bundle.js
@@ -0,0 +1,18 @@
+(self["webpackChunksto_do"] = self["webpackChunksto_do"] || []).push([["taskUI"],{
+
+/***/ "./src/taskUI.js":
+/*!***********************!*\
+  !*** ./src/taskUI.js ***!
+  \***********************/
+/***/ (() => {
+
+
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/taskUI.js"));
+/******/ }
+]);
