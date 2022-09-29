0eff9d3cce0acc71d3bd75cb8166d2909436ebcd
diaz <jerylfranz460@gmail.com>
2022-09-29 13:58:54 +0400

    function to create ProjectUI that holds the task within that project

---
+++ src/projectUI.js
@@ -0,0 +1,14 @@
+let i = 0
+export function createProjectUI(container) {
+  i++
+  const projectContainer = document.createElement('div');
+  const projTitleCont = document.createElement('div')
+  const projectTitle = document.createElement('h3');
+  projTitleCont.appendChild(projectTitle)
+  const projTaskCont = document.createElement('div')
+  const projAddTaskBtn = document.createElement('button')
+  projectContainer.append(projTitleCont, projAddTaskBtn, projTaskCont)
+  projectTitle.textContent = `Project-${i}`
+
+  container.appendChild(projectContainer);
+}
