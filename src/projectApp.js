19d5dfbd21a2b8f2b7ca9985fea1946ef4092b60
zaid <jerylfranz460@gmail.com>
2022-09-29 10:14:53 +0400

    appended button to project container

--- src/projectApp.js
+++ src/projectApp.js
@@ -1,9 +1,16 @@
 
 const todayHeader = document.getElementById("header-text"); //style
 const projectContainer = document.createElement('div');
-const mainContainer = document.getElementById('main-content-container')
+const mainContainer = document.getElementById('main-content-container');
+const addProjectBtn = document.createElement('button');
+const addProjectBtnContainer = document.createElement('div');
 export function createProjectAppUI() {
-  todayHeader.textContent = 'PROJECTS'
+  addProjectBtnContainer.setAttribute('id', 'add-project-btn-container');
+  addProjectBtn.setAttribute('id', 'add-project-btn');
+  addProjectBtn.textContent = 'ADD PROJECT';
+  addProjectBtnContainer.appendChild(addProjectBtn);
   projectContainer.setAttribute('id', 'project-app-container')
+  projectContainer.appendChild(addProjectBtnContainer)
   mainContainer.appendChild(projectContainer)
+  todayHeader.textContent = 'PROJECTS'
 }
